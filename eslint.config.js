648c5cee6266f1e9de586b2e86841896e9938ff6
Maciek Sakrejda <m.sakrejda@gmail.com>
2024-12-17 21:50:35 -0800

    Finish setting up linting

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {ignores: ["dist/**"]},
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
 ];
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { useCountUp } from "./hooks";
 
 const App = () => {
